f6e10d3a6b9abf3472bad3822d60a76007da892d
Philippe57310 <philippe.pham@cegetel.net>
2021-04-26 12:23:51 +0200

    6

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View(_context.Items);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var item = _context.Items.FirstOrDefault(i => i.Id == Id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
