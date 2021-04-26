066a25dbb58cfd2213b1b773bd5d27c0d9ae45ce
Philippe57310 <philippe.pham@cegetel.net>
2021-04-26 10:28:10 +0200

    4b

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -9,7 +9,6 @@ namespace WishList.Data
 {
     public class ApplicationDbContext: DbContext
     {
-
         public DbSet<Item> Items { get; set; }
         public ApplicationDbContext(DbContextOptions options): base(options)
         {
